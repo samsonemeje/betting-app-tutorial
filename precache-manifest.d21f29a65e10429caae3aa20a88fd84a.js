1f4ad38049dd42c79a103dcba4e3667234020f54
samsonemeje <samsonemeje@gmail.com>
2020-05-20 01:17:22 +0100

    Updates

---
+++ precache-manifest.d21f29a65e10429caae3aa20a88fd84a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f8db0ef6b12ee64b6289419e12b32d",
+    "url": "/betting-app-tutorial/index.html"
+  },
+  {
+    "revision": "e7e82a42dfb118ae2a70",
+    "url": "/betting-app-tutorial/static/css/main.42893ef0.chunk.css"
+  },
+  {
+    "revision": "01bcb4d4c70839923a08",
+    "url": "/betting-app-tutorial/static/js/2.03a9dd37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/betting-app-tutorial/static/js/2.03a9dd37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e82a42dfb118ae2a70",
+    "url": "/betting-app-tutorial/static/js/main.08092029.chunk.js"
+  },
+  {
+    "revision": "eb8220ae6006f534700d",
+    "url": "/betting-app-tutorial/static/js/runtime-main.0f5ea136.js"
+  }
+]);
